32c1903cbb195e73139a8af0c89456cc68aeec52
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-09-22 04:19:10 +0300

    add fullpage.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,14 @@
 /**
  * An example ES6 module:
  */
+import fullpage from 'fullpage.js';
 import pasteSVG from './utils/pasteSvg';
 import sayHello from './utils/sayHello';
 sayHello();
 pasteSVG();
+
+new fullpage('#fullpage', {
+	//options here
+});
+
+//methods
