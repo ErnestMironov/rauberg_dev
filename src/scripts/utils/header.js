3ef50671a2c9b4017f9f054050efe12927d04629
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-09-28 02:50:46 +0300

    header fix

--- src/scripts/utils/header.js
+++ src/scripts/utils/header.js
@@ -1,30 +1,51 @@
 export default function header() {
     (function () {
-        const btn = document.querySelector(`.search-box`),
-            input = document.querySelector(`.search-box input`);
+        const searchBox = document.querySelector('.search-box'),
+            input = document.querySelector('.search-box input'),
+            closeBtn = document.querySelector('.search-box__close');
 
-        btn.addEventListener(`click`, e => {
-            console.log(document.activeElement);
-            if (input !== document.activeElement) {
+        function close() {
+            setTimeout(() => {
+                if (searchBox.classList.contains(`active`)) {
+                    searchBox.classList.remove(`active`);
+                    input.value = ``;
+                    input.blur();
+                    closeBtn.removeEventListener(`click`, close)
+                }
+            }, 200)
+        };
+
+        searchBox.addEventListener(`click`, e => {
+            setTimeout(() => {
+                searchBox.classList.add(`active`)
                 input.focus()
+                closeBtn.addEventListener(`click`, close)
+            })
+        });
+
+        document.addEventListener(`click`, e => {
+            if (!searchBox.contains(e.target)) {
+                close();
             }
         })
     }());
-    (function () {
-        const searchBox = document.querySelectorAll('.search-box input[type="text"] + span');
-
-        searchBox.forEach((elm) => {
-            elm.addEventListener('click', () => {
-                elm.previousElementSibling.value = '';
-            });
-        });
-    }());
 
     (function () {
-        const trigger = document.querySelector(`.products-trigger`)
+        const trigger = document.querySelector(`.products-trigger`),
+            popup = document.querySelector(`.prod-popup`),
+            closeBtn = document.querySelector(`.prod-popup__close`)
+
+        function close() {
+            popup.classList.remove(`active`);
+            trigger.classList.remove(`active`);
+
+        };
+
+        closeBtn.addEventListener(`click`, close);
 
         trigger.addEventListener(`click`, e => {
             trigger.classList.toggle('active')
+            popup.classList.add(`active`)
         })
     }());
 
