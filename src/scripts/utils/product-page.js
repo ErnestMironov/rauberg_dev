f889f34f32b10d9d00f133718d43d6e4a75e9886
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-10-16 23:49:15 +0300

    pp fix

--- src/scripts/utils/product-page.js
+++ src/scripts/utils/product-page.js
@@ -2,13 +2,16 @@ import $ from 'jquery';
 import tabs from 'tabs';
 
 export default function () {
-    $(".js-add-review").on("click", function () {
-        $("html, body").animate({
-            scrollTop: $(".form").offset().top - 90
-        }, 500),
-            $(".form input[type=text]").get(0).focus()
-    });
-
     var container = document.querySelector('.tab-container')
-    tabs(container);
+    if (container) {
+        $(".js-add-review").on("click", function () {
+            $("html, body").animate({
+                scrollTop: $(".form").offset().top - 90
+            }, 500),
+                $(".form input[type=text]").get(0).focus()
+        });
+
+        tabs(container);
+
+    }
 }
